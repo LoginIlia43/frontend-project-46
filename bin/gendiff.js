65930f0de79b7aafb7ede21fb123ac224e00399c
Ilia Loginov <loginoviv43@gmail.com>
2022-11-06 13:47:38 +0300

    add compareFilesData.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import getFileData from '../getFileData.js';
+import compareFilesData from '../compareFilesData.js';
 
 const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .argument('<file1>')
+  .argument('<file2>')
+  .action((file1, file2) => {
+    const data1 = getFileData(file1);
+    const data2 = getFileData(file2);
+    console.log(compareFilesData(data1, data2))
+  });
 program.parse();
