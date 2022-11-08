58cfe1cdae8dff3cff9668bb6c303077480b4ab5
Ilia Loginov <loginoviv43@gmail.com>
2022-11-08 13:34:55 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import compareFilesData from './src/compareFilesData.js';
+import { getFileData, getFullFilePath } from './src/getFileData.js';
 
-export default compareFilesData;
+const gendiff = (file1, file2) => {
+    const file1Path = getFullFilePath(file1);
+    const file2Path = getFullFilePath(file2);
+    const data1 = getFileData(file1Path);
+    const data2 = getFileData(file2Path);
+    const result = compareFilesData(data1, data2);
+
+    return result;
+} 
+
+export default gendiff;
