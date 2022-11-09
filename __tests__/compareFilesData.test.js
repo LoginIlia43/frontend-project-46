df3856ffcba21e19633533d899f5a0b47f96ef44
Ilia Loginov <loginoviv43@gmail.com>
2022-11-09 23:18:01 +0300

    update tests

--- __tests__/compareFilesData.test.js
+++ __tests__/compareFilesData.test.js
@@ -9,9 +9,15 @@ const __dirname = dirname(__filename);
 
 const filePath = (fileName) => `${__dirname}/__fixtures__/${fileName}`;
 
-const file1 = parsers(filePath('file1.json'));
-const file2 = parsers(filePath('file2.json'));
+const file1Json = parsers(filePath('file1.json'));
+const file2Json = parsers(filePath('file2.json'));
+const file1Yaml = parsers(filePath('file1.yaml'));
+const file2Yml = parsers(filePath('file2.yml'));
 
-test('test1', () => {
-  expect(compareFilesData(file1, file2)).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+test('compare_json', () => {
+  expect(compareFilesData(file1Json, file2Json)).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
+});
+
+test('compare_yaml_yml', () => {
+  expect(compareFilesData(file1Yaml, file2Yml)).toBe('{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}');
 });
