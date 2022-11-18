75f63c09f8bbcf50e2cd00f685ab967dbe964655
Ilia Loginov <loginoviv43@gmail.com>
2022-11-18 18:45:18 +0300

    add stylish.test.js

---
+++ __tests__/stylish.test.js
@@ -0,0 +1,64 @@
+import { expect, test } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import getDiff from "../src/getDiff.js"
+import stylish from "../src/formatters/stylish.js"
+import parsers from "../src/parsers.js"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = (fileName) => `${__dirname}/__fixtures__/${fileName}`;
+
+const file3 = parsers(filePath('file3.json'));
+const file4 = parsers(filePath('file4.json'));
+const diff = getDiff(file3, file4);
+
+const res = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`
+
+test ('stylish', () => {
+  expect (stylish(diff)).toBe(res)
+});
