6609607ca7e4ed58efef32081081100f502a8a4f
Ilia Loginov <loginoviv43@gmail.com>
2022-11-18 18:36:17 +0300

    update gendiff, add stylish format for console

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,10 +7,10 @@ const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<file1>')
   .argument('<file2>')
   .action((file1, file2) => {
-    console.log(gendiff(file1, file2));
+    console.log(gendiff(file1, file2, program.opts().format));
   });
 program.parse();
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-import compareFilesData from './src/compareFilesData.js';
 import parsers from './src/parsers.js';
 import { getFullFilePath } from './src/getFileData.js';
+import getDiff from './src/getDiff.js';
+import stylish from './src/formatters/stylish.js';
 
-const gendiff = (file1, file2) => {
+const gendiff = (file1, file2, format = 'stylish') => {
   const file1Path = getFullFilePath(file1);
   const file2Path = getFullFilePath(file2);
   const data1 = parsers(file1Path);
   const data2 = parsers(file2Path);
-  const result = compareFilesData(data1, data2);
+  const diff = getDiff (data1, data2);
 
-  return result;
+  if (format === 'stylish') {
+    return stylish(diff);
+  };
 };
 
 export default gendiff;
