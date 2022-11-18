b58407d367da520550dbd2801c110d23266b5bac
Ilia Loginov <loginoviv43@gmail.com>
2022-11-18 14:34:23 +0300

    add getDiff func

--- src/getDiff.js
+++ src/getDiff.js
@@ -5,18 +5,18 @@ const getDiff = (data1, data2) => {
 
   const diff = keys.map(key => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return { key, type: 'nested', children: getDiff(data1[key], data1[key]) };
+      return { key, type: 'nested', children: getDiff(data1[key], data2[key]) };
     }
     if (!Object.hasOwn(data1, key)) {
-      return { key, type: 'added', value2: data2[key] };
+      return { key, type: 'added', value: data2[key] };
     }
     if (!Object.hasOwn(data2, key)) {
-      return { key, type: 'deleted', value1: data1[key] };
+      return { key, type: 'deleted', value: data1[key] };
     }
     if (data1[key] !== data2[key]) {
-      return { key, type: 'updated', value1: data1[key], value2: data2[key]};
+      return { key, type: 'updated', value1: data1[key], value2: data2[key] };
     }
-    return { key, type: 'unchanged', value1: data1[key]};
+    return { key, type: 'unchanged', value1: data1[key] };
   })
 
   return diff;
