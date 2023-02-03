48067515e328966b0a34009c190ce408c6d5f472
Ilia Loginov <loginoviv43@gmail.com>
2023-02-03 02:37:50 +0100

    remove console.log

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import formatter from './src/formatters/index.js';
 const gendiff = (file1 = 0, file2 = 0, format = 'stylish') => {
   const file1Path = getPath(file1);
   const file2Path = getPath(file2);
-  console.log(process.cwd());
   const data1 = parsers(file1Path);
   const data2 = parsers(file2Path);
   const diff = getDiff(data1, data2);
